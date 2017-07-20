85cca17675f89a6d6e439e9d032d3bb0027d47bc
Woeler <woeler@esoleaderboards.com>
2017-07-20 18:45:26 +0200

    Added TYPO3 DocBlock

--- Classes/ViewHelpers/Form/MauticPropertiesViewHelper.php
+++ Classes/ViewHelpers/Form/MauticPropertiesViewHelper.php
@@ -3,12 +3,18 @@
 declare(strict_types=1);
 
 /*
- * @copyright   2017 Mautic Contributors. All rights reserved
- * @author      Mautic
+ * This extension was developed by Beech.it
  *
- * @link        http://mautic.org
+ * This file is part of the TYPO3 CMS project.
  *
- * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 3
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
  */
 
 namespace Mautic\MauticTypo3\ViewHelpers\Form;
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,18 @@
 <?php
 
 /*
- * @copyright   2017 Mautic Contributors. All rights reserved
- * @author      Mautic
+ * This extension was developed by Beech.it
  *
- * @link        http://mautic.org
+ * This file is part of the TYPO3 CMS project.
  *
- * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 3
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
  */
 
 $EM_CONF[$_EXTKEY] = [
@@ -18,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearcacheonload' => 0,
     'author'           => 'Woeler',
     'author_email'     => 'woeler@esoleaderboards.com',
-    'author_company'   => 'Mautic',
+    'author_company'   => 'Beech.it',
     'constraints'      => [
         'depends' => [
             'typo3' => '8.7.0-8.7.99',
