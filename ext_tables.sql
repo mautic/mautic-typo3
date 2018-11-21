db12a8f78099f2a63b2e9a479b50904b100dbe34
Woeler <woeler@woeler.eu>
2018-11-21 16:32:33 +0100

    [CLEANUP] Clean repository for new code

--- ext_tables.sql
+++
@@ -1,7 +0,0 @@
-#
-# Table structure for table 'tt_content'
-#
-CREATE TABLE tt_content (
-    mautic_dynamic_content_slot_name varchar(255) DEFAULT '' NOT NULL,
-    mautic_form_id int(11) unsigned DEFAULT '0',
-);
