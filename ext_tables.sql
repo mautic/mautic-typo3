3348c52a53a5ec78d86029bc3661023b4a0b6c56
Florian Wessels <f.wessels@bitmotion.de>
2018-11-23 17:07:04 +0100

    [CLEANU-UP] Remove unnecessary sql code

--- ext_tables.sql
+++ ext_tables.sql
@@ -9,17 +9,8 @@ CREATE TABLE tx_marketingautomation_persona (
 # Table structure for table 'tx_marketingautomation_segment'
 #
 CREATE TABLE tx_marketingautomation_segment (
-    uid int(11) NOT NULL auto_increment,
-    pid int(11) DEFAULT '0' NOT NULL,
-    tstamp int(11) DEFAULT '0' NOT NULL,
-    crdate int(11) DEFAULT '0' NOT NULL,
-    cruser_id int(11) DEFAULT '0' NOT NULL,
-    deleted tinyint(4) DEFAULT '0' NOT NULL,
-
     title varchar(255) DEFAULT '' NOT NULL,
     items int(11) DEFAULT '0' NOT NULL,
-
-    PRIMARY KEY (uid)
 );
 
 #
