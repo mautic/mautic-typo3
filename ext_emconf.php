042143f837bf6818e81a99500d42575876d045ae
Florian Wessels <flo.wessels@gmail.com>
2020-04-24 16:20:44 +0200

    [FEATURE] Introduce compatibility for TYPO3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.1.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.0-10.4.99',
             'marketing_automation' => '',
         ],
         'conflicts' => [],
--- ext_tables.php
+++ ext_tables.php
@@ -1,30 +1,36 @@
 <?php
 defined('TYPO3_MODE') || die;
 
-call_user_func(function () {
-    // Assign the hooks for pushing newly created and edited forms to Mautic
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDuplicate'][1489959059] =
-        \Bitmotion\Mautic\Hooks\MauticFormHook::class;
+call_user_func(
+    function ($extensionKey) {
+        // Assign the hooks for pushing newly created and edited forms to Mautic
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDuplicate'][1489959059] =
+            \Bitmotion\Mautic\Hooks\MauticFormHook::class;
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDelete'][1489959059] =
-        \Bitmotion\Mautic\Hooks\MauticFormHook::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDelete'][1489959059] =
+            \Bitmotion\Mautic\Hooks\MauticFormHook::class;
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormSave'][1489959059] =
-        \Bitmotion\Mautic\Hooks\MauticFormHook::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormSave'][1489959059] =
+            \Bitmotion\Mautic\Hooks\MauticFormHook::class;
 
-    // Backend Module
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Bitmotion.Mautic',
-        'tools',
-        'api',
-        'bottom',
-        [
-            'Backend' => 'show, save'
-        ],
-        [
-            'access' => 'admin',
-            'icon' => 'EXT:mautic/Resources/Public/Icons/mautic-with-background.png',
-            'labels' => 'LLL:EXT:mautic/Resources/Private/Language/locallang_mod.xlf',
-        ]
-    );
-});
+        // Backend Module
+        if (version_compare(TYPO3_version, '10.0.0', '<')) {
+            $extensionName = 'Bitmotion.Mautic';
+            $controllerName = 'Backend';
+        }
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            $extensionName ?? $extensionKey,
+            'tools',
+            'api',
+            'bottom',
+            [
+                $controllerName ?? \Bitmotion\Mautic\Controller\BackendController::class => 'show, save'
+            ],
+            [
+                'access' => 'admin',
+                'icon' => 'EXT:mautic/Resources/Public/Icons/mautic-with-background.png',
+                'labels' => 'LLL:EXT:mautic/Resources/Private/Language/locallang_mod.xlf',
+            ]
+        );
+}, 'mautic');
